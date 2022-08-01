649e9d2815ab536034fc34f9676c5c944b24783f
MrVaas <guilian.mercier@gmail.com>
2022-08-01 17:14:38 +0200

    THE END?

---
+++ perks.py
@@ -0,0 +1,26 @@
+def getperks(lvl,usrclasse,usrrace):
+	if usrrace == "Orc".casefold():
+		if usrclasse == "Chasseur".casefold():
+		elif usrclasse == "Chaman".casefold():
+		elif usrclasse == "Guerrier".casefold():
+		elif usrclasse == "Berserk".casefold():
+	elif usrrace == "Humain".casefold():
+		if usrclasse == "Archer".casefold():
+		elif usrclasse == "Moine".casefold():
+		elif usrclasse == "Voleur".casefold():
+		elif usrclasse == "Barde".casefold():
+	elif usrrace == "Elf".casefold():
+		if usrclasse == "Rodeur".casefold():
+		elif usrclasse == "Elementaliste".casefold():
+		elif usrclasse == "Sentinelle".casefold():
+		elif usrclasse == "Gardien des runes".casefold():
+	elif usrrace == "Centaure".casefold():
+		if usrclasse == "Archer".casefold():
+		elif usrclasse == "Sorcier".casefold():
+		elif usrclasse == "Dueliste".casefold():
+		elif usrclasse == "Lancier".casefold():
+	elif usrrace == "Demon".casefold():
+		if usrclasse == "Necromancien".casefold():
+		elif usrclasse == "Arcaniste".casefold():
+		elif usrclasse == "Revenant".casefold():
+		elif usrclasse == "Maitre du savoir".casefold():
